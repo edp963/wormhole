d28921c136c17a32d6aa6d7247f211bb6987a4b9
weidongduan <weidongduan@creditease.cn>
2017-11-07 14:58:19 +0800

    fix wrong minTs bug when slit file by (24) hours

--- sparkx/src/main/scala/edp/wormhole/hdfslog/HdfsMainProcess.scala
+++ sparkx/src/main/scala/edp/wormhole/hdfslog/HdfsMainProcess.scala
@@ -306,6 +306,8 @@ object HdfsMainProcess extends EdpLogging {
           correctFileName = name
           currentCorrectMetaContent = meta
           correctCurrentSize = 0
+          finalMinTs = minTs
+          finalMaxTs = maxTs
         }
 
         if (minTs != null && correctFileName != null) {
@@ -320,6 +322,8 @@ object HdfsMainProcess extends EdpLogging {
             correctFileName = name
             currentCorrectMetaContent = meta
             correctCurrentSize = 0
+            finalMinTs = minTs
+            finalMaxTs = maxTs
           }
         }
 
