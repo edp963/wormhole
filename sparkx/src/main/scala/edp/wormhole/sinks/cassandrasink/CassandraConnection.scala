63695f8f603bef3e1dcc1abe1d9c030704a0e3a1
xinghanyu <xinghanyu@creditease.cn>
2017-11-20 17:14:06 +0800

    Pom files and cassandra sink.

--- sparkx/src/main/scala/edp/wormhole/sinks/cassandrasink/CassandraConnection.scala
+++ sparkx/src/main/scala/edp/wormhole/sinks/cassandrasink/CassandraConnection.scala
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,13 +22,9 @@
 package edp.wormhole.sinks.cassandrasink
 
 import java.net.InetAddress
-import javax.naming.AuthenticationException
 
 import com.datastax.driver.core.Cluster.Builder
-import com.datastax.driver.core.exceptions.NoHostAvailableException
 import com.datastax.driver.core.{Cluster, Session}
-import edp.wormhole.memorystorage.ConfMemoryStorage._
-import edp.wormhole.sinks.SinkProcessConfig
 import edp.wormhole.spark.log.EdpLogging
 
 import scala.collection.mutable
