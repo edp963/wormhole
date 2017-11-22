7ad9ae1bd59f82e9e8ca6857b098ef6c8be48c0e
Xiaoyan <1223117836@qq.com>
2017-11-22 09:54:27 +0800

    session get latest project ids

--- rider/rider-server/src/main/scala/edp/rider/rest/util/AuthorizationProvider.scala
+++ rider/rider-server/src/main/scala/edp/rider/rest/util/AuthorizationProvider.scala
@@ -27,11 +27,12 @@ import edp.rider.common.RiderLogger
 import edp.rider.module.{ConfigurationModuleImpl, PersistenceModuleImpl}
 import edp.rider.rest.persistence.entities.User
 import edp.rider.rest.router.{LoginClass, LoginResult, SessionClass}
+import edp.rider.rest.util.CommonUtils.minTimeOut
 import slick.jdbc.MySQLProfile.api._
 
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 
 sealed abstract class AuthorizationError extends Exception {
   val statusCode = Unauthorized
@@ -73,6 +74,11 @@ object AuthorizationProvider extends ConfigurationModuleImpl with PersistenceMod
       case e: AuthorizationError => Future.successful(Left(e))
     }
 
+  }
+
+  def genCurrentSession(session: SessionClass): SessionClass = {
+    val projectIds = Await.result(relProjectUserDal.findByFilter(_.userId === session.userId), minTimeOut).map(_.projectId)
+    SessionClass(session.userId, projectIds.toList, session.roleType)
   }
 
   def authorize(credentials: Credentials): Future[Option[SessionClass]] =
