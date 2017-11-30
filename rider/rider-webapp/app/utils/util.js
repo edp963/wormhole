13ccc805405b805282d9563c63bb4e8f87b334f2
wangfuying <fuyingwang2@creditease.cn>
2017-11-30 15:05:54 +0800

    transformation sql bug

--- rider/rider-webapp/app/utils/util.js
+++ rider/rider-webapp/app/utils/util.js
@@ -151,3 +151,19 @@ export function isJSONNotEmpty (str) {
   }
   return false
 }
+
+/**
+ * flow transformation: sql语句可能出现双引号，从而影响Json对象解析，将双引号转义
+ * @param sql
+ */
+export function preProcessSql (sql) {
+  const doubleQuotationRegex = new RegExp('\\"{1}', 'g')
+  let result
+  if (sql.indexOf('\\') > -1) {
+    result = sql
+  } else {
+    result = sql.replace(doubleQuotationRegex, '\\"')
+  }
+  return result
+}
+
