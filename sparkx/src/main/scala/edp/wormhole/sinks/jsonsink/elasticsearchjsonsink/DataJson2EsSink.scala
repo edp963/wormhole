b624a910703c899795a2dddf994118fc937dac39
weidongduan <weidongduan@creditease.cn>
2017-12-15 16:21:36 +0800

    es kafka mongo json finished

--- sparkx/src/main/scala/edp/wormhole/sinks/jsonsink/elasticsearchjsonsink/DataJson2EsSink.scala
+++ sparkx/src/main/scala/edp/wormhole/sinks/jsonsink/elasticsearchjsonsink/DataJson2EsSink.scala
@@ -1,16 +1,17 @@
-package edp.wormhole.sinks.elasticsearchJsonSink
+package edp.wormhole.sinks.jsonsink.elasticsearchjsonsink
 
 import java.util.UUID
 
-import edp.wormhole.common.util.JsonUtils._
 import com.alibaba.fastjson.{JSON, JSONArray, JSONObject}
 import edp.wormhole.common.ConnectionConfig
+import edp.wormhole.common.util.JsonUtils._
 import edp.wormhole.sinks.SourceMutationType.INSERT_ONLY
+import edp.wormhole.sinks.jsonsink.JsonParseHelper
 import edp.wormhole.sinks.{SinkProcessConfig, SinkProcessor, SourceMutationType}
 import edp.wormhole.spark.log.EdpLogging
 import edp.wormhole.ums.UmsFieldType.UmsFieldType
-import edp.wormhole.ums.{UmsFieldType, UmsNamespace, UmsSysField}
 import edp.wormhole.ums.UmsProtocolType.UmsProtocolType
+import edp.wormhole.ums.{UmsFieldType, UmsNamespace, UmsSysField}
 import org.json4s.JValue
 
 import scala.collection.mutable
@@ -54,7 +55,7 @@ class DataJson2EsSink extends SinkProcessor with EdpLogging {
   private def insertOrUpdate(tupleList: Seq[Seq[String]], targetSchemaArr: JSONArray, sinkMap: collection.Map[String, (Int, UmsFieldType, Boolean)], sinkConfig: SinkProcessConfig, sinkNamespace: String, cc: ConnectionConfig): Boolean = {
     val dataList = ListBuffer.empty[(String, Long, String)]
     for (row <- tupleList) {
-      val jsonData = jsonObjHelper(row, sinkMap, targetSchemaArr)
+      val jsonData = JsonParseHelper.jsonObjHelper(row, sinkMap, targetSchemaArr)
       val umsId = jsonData.getLong(UmsSysField.ID.toString)
       val data = jsonData.toJSONString
       val _ids = ListBuffer.empty[String]
@@ -120,7 +121,7 @@ class DataJson2EsSink extends SinkProcessor with EdpLogging {
     val insertList = ListBuffer.empty[String]
     if (tupleList.nonEmpty) {
       for (row <- tupleList) {
-        val data = jsonObjHelper(row, sinkMap, targetSchemaArr).toJSONString
+        val data = JsonParseHelper.jsonObjHelper(row, sinkMap, targetSchemaArr).toJSONString
         val uuid = UUID.randomUUID().toString
         insertList += s"""{ "$optNameInsert" : {"_id" : "${uuid}" }}"""
         insertList += data
@@ -138,74 +139,6 @@ class DataJson2EsSink extends SinkProcessor with EdpLogging {
     val responseContent = doHttp(url, connectionConfig.username, connectionConfig.password, requestContent)
     val responseJson: JValue = json2jValue(responseContent)
     checkResponseSuccess(responseJson)
-  }
-
-
-  private def jsonObjHelper(tuple: Seq[String], schemaMap: collection.Map[String, (Int, UmsFieldType, Boolean)], subFields: JSONArray): JSONObject = {
-    val outputJson = new JSONObject()
-    val size = subFields.size()
-    for (i <- 0 until size) {
-      val jsonObj = subFields.getJSONObject(i)
-      val name = jsonObj.getString("name")
-      val dataType = jsonObj.getString("type")
-      if (schemaMap.contains(name)) {
-        val subFields: Option[JSONArray] = if (jsonObj.containsKey("sub_fields")) Some(jsonObj.getJSONArray("sub_fields")) else None
-        val value = str2Json(name, tuple(schemaMap(name)._1), dataType, subFields)
-        outputJson.put(name, value)
-      } else {
-        assert(dataType == "jsonobject", "name: " + name + " not found, it should be jsonobject, but it is " + dataType)
-        val subFields: JSONArray = jsonObj.getJSONArray("sub_fields")
-        val subJsonObj: JSONObject = jsonObjHelper(tuple, schemaMap, subFields)
-        outputJson.put(name, subJsonObj)
-      }
-    }
-    outputJson
-  }
-
-  private def str2Json(outerName: String, data: String, dataType: String, subFieldsOption: Option[JSONArray]): Any = {
-    if (dataType == "jsonobject") {
-      val jsonData = JSON.parseObject(data)
-      val outputJson = new JSONObject()
-      val subFields = subFieldsOption.get
-      val size = subFields.size()
-      for (i <- 0 until size) {
-        val jsonObj = subFields.getJSONObject(i)
-        val name = jsonObj.getString("name")
-        val subDataType = jsonObj.getString("type")
-        val subData = jsonData.getString(name)
-        val subSubFields = if (jsonObj.containsKey("sub_fields")) Some(jsonObj.getJSONArray("sub_fields")) else None
-        val subResult: Any = str2Json(name, subData, subDataType, subSubFields)
-        outputJson.put(name, subResult)
-      }
-      outputJson
-    } else if (dataType == "jsonarray") {
-      val jsonArray = JSON.parseArray(data)
-      val jsonArraySubFields = subFieldsOption.get
-      val dataSize = jsonArray.size()
-      val schemaSize = jsonArraySubFields.size()
-      val result = new JSONArray()
-      for (i <- 0 until dataSize) {
-        val outputJson = new JSONObject()
-        for (j <- 0 until schemaSize) {
-          val schemaObj = jsonArraySubFields.getJSONObject(j)
-          val value = str2Json(schemaObj.getString("name"), jsonArray.getJSONObject(i).get(schemaObj.getString("name")).toString, schemaObj.getString("type"), if (schemaObj.containsKey("sub_fields")) Some(schemaObj.getJSONArray("sub_fields")) else None)
-          outputJson.put(schemaObj.getString("name"), value)
-        }
-        result.add(outputJson)
-      }
-      result
-    } else if (dataType.endsWith("array")) {
-      JSON.parseArray(data)
-    } else {
-      if (outerName == UmsSysField.ACTIVE.toString)
-        data match {
-          case "i" | "u" => 1
-          case "d" => 0
-          case _ => -1
-        } else {
-        parseData2CorrectType(dataType, data)
-      }
-    }
   }
 
   private def checkResponseSuccess(responseJson: JValue): Boolean = {
@@ -275,16 +208,5 @@ class DataJson2EsSink extends SinkProcessor with EdpLogging {
       }
     }
     (queryResult, esid2VersionMap)
-  }
-
-  private def parseData2CorrectType(dataType: String, data: String): Any = {
-    dataType.toLowerCase match {
-      case "int" => data.toInt
-      case "long" => data.toLong
-      case "double" => data.toDouble
-      case "float" => data.toFloat
-      case "boolean" => data.toBoolean
-      case _ => data
-    }
   }
 }
