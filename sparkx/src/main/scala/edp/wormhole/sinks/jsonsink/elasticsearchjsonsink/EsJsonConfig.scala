bdff2fd982c264dac2c56a42cc6122b650913cb5
pingzhao3 <pingzhao3@creditease.cn>
2017-12-28 14:24:36 +0800

    json sink Restructure

--- sparkx/src/main/scala/edp/wormhole/sinks/jsonsink/elasticsearchjsonsink/EsJsonConfig.scala
+++
@@ -1,7 +0,0 @@
-package edp.wormhole.sinks.jsonsink.elasticsearchjsonsink
-
-import edp.wormhole.sinks.SourceMutationType
-
-case class EsJsonConfig(`mutation_type`: Option[String]) {
-  lazy val `mutation_type.get` = `mutation_type`.getOrElse(SourceMutationType.I_U_D.toString)
-}
