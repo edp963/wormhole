7a54fa1763e45317565774ad5d29d5081116a674
pingzhao3 <pingzhao3@creditease.cn>
2017-12-28 14:24:36 +0800

    json sink Restructure

--- sparkx/src/main/scala/edp/wormhole/sinks/jsonsink/mongojsonsink/MongoJsonConfig.scala
+++
@@ -1,8 +0,0 @@
-package edp.wormhole.sinks.jsonsink.mongojsonsink
-
-import edp.wormhole.sinks.SourceMutationType
-
-case class MongoJsonConfig(`mutation_type`: Option[String]) {
-  lazy val `mutation_type.get` = `mutation_type`.getOrElse(SourceMutationType.I_U_D.toString)
-}
-
