6b97155c39fec642642aed0240d33ed3a0e1226a
zp <85016910@qq.com>
2018-01-15 12:00:11 +0800

    add umsts save as string type

--- sparkx/src/main/scala/edp/wormhole/sinks/hbasesink/HbaseConfig.scala
+++ sparkx/src/main/scala/edp/wormhole/sinks/hbasesink/HbaseConfig.scala
@@ -28,6 +28,7 @@ import edp.wormhole.ums.UmsSysField
 case class HbaseConfig(`hbase.columnFamily`: Option[String],
                        `hbase.znParent`: Option[String],
                        `hbase.saveAsString`: Option[Boolean],
+                       `umsTs.saveAsString`: Option[Boolean],
                        `hbase.version.column`:Option[String],
                        `mutation_type`:Option[String],
                        `hbase.rowKey`: String //separator","
@@ -36,6 +37,7 @@ case class HbaseConfig(`hbase.columnFamily`: Option[String],
   lazy val `hbase.columnFamily.get` = `hbase.columnFamily`.getOrElse("cf")
  // lazy val `hbase.znParent.get` = `hbase.znParent`.getOrElse("/hbase")
   lazy val `hbase.valueType.get` = `hbase.saveAsString`.getOrElse(false)
+  lazy val `umsTs.valueType.get` = `umsTs.saveAsString`.getOrElse(true)
   lazy val `mutation_type.get` = `mutation_type`.getOrElse(SourceMutationType.I_U_D.toString)
   lazy val `hbase.version.column.get` = `hbase.version.column`.getOrElse(UmsSysField.TS.toString)
 }
