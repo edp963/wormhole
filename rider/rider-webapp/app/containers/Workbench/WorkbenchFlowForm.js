cf6ec7dad9f014be9631d2e65ace6a752d3404bf
wangfuying <fuyingwang2@creditease.cn>
2018-01-19 18:11:54 +0800

    namespace source schema tuple类型个数不限制；添加 data system 类型

--- rider/rider-webapp/app/containers/Workbench/WorkbenchFlowForm.js
+++ rider/rider-webapp/app/containers/Workbench/WorkbenchFlowForm.js
@@ -200,7 +200,8 @@ export class WorkbenchFlowForm extends React.Component {
       { value: 'kafka', icon: 'icon-kafka', style: {fontSize: '35px'} },
       { value: 'postgresql', icon: 'icon-postgresql', style: {fontSize: '31px'} },
       { value: 'cassandra', icon: 'icon-cass', style: {fontSize: '52px', lineHeight: '60px'} },
-      { value: 'mongodb', icon: 'icon-mongodb', style: {fontSize: '26px'} }
+      { value: 'mongodb', icon: 'icon-mongodb', style: {fontSize: '26px'} },
+      { value: 'vertica', icon: 'icon-vertica', style: {fontSize: '45px'} }
     ]
 
     let formValues = this.props.form.getFieldsValue([
