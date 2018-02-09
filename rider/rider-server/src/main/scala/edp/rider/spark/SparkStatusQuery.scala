73e2c80032a8a429da386078e9e54c0907e6eed5
Xiaoyan <1223117836@qq.com>
2018-02-09 19:02:05 +0800

    job used namespace not contains project, disable actions are start/renew/modify

--- rider/rider-server/src/main/scala/edp/rider/spark/SparkStatusQuery.scala
+++ rider/rider-server/src/main/scala/edp/rider/spark/SparkStatusQuery.scala
@@ -45,9 +45,9 @@ object SparkStatusQuery extends RiderLogger {
     modules.jobDal.updateJobStatus(job.id, appInfo)
     //    val startedTime = if (appInfo.startedTime != null) Some(appInfo.startedTime) else Some("")
     //    val stoppedTime = if (appInfo.finishedTime != null) Some(appInfo.finishedTime) else Some("")
-    FullJobInfo(Job(job.id, job.name, job.projectId, job.sourceNs, job.sinkNs, job.sourceType, job.sparkConfig, job.startConfig, job.eventTsStart, job.eventTsEnd, job.sourceConfig,
+    val newJob = Job(job.id, job.name, job.projectId, job.sourceNs, job.sinkNs, job.sourceType, job.sparkConfig, job.startConfig, job.eventTsStart, job.eventTsEnd, job.sourceConfig,
       job.sinkConfig, job.tranConfig, appInfo.appState, Some(appInfo.appId), job.logPath, Option(appInfo.startedTime), Option(appInfo.finishedTime), job.createTime, job.createBy, job.updateTime, job.updateBy)
-      , projectName, getDisableAction(JobStatus.jobStatus(job.status)))
+    FullJobInfo(newJob, projectName, getDisableAction(newJob))
   })
 
 
