421400379c53656d473660342b2b5e1508043611
Xiaoyan <1223117836@qq.com>
2018-03-05 19:36:01 +0800

    job config update

--- sparkx/src/main/scala/edp/wormhole/batchjob/BatchJobConfig.scala
+++ sparkx/src/main/scala/edp/wormhole/batchjob/BatchJobConfig.scala
@@ -44,7 +44,8 @@ case class SinkConfig(sinkNamespace: String,
                       classFullName: Option[String],
                       specialConfig: Option[String],
                       tableKeys: Option[String],
-                      projection:Option[String])
+                      projection:Option[String],
+                      sink_protocol: Option[String] = None)
 
 case class JobConfig(appName: String,
                      master: String,
