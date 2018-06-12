d55f1128b61cd83615d41ab93ee8d49e97e9f12b
Xiaoyan <1223117836@qq.com>
2018-06-12 16:34:27 +0800

    pom version update to 0.4.2

--- rider/bin/mad-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export MAD_HOME=/app/wormhole-test
-echo "MAD_HOME" $MAD_HOME
-java -DMAD_HOME=/app/wormhole-test -cp $MAD_HOME/lib/wormhole-mad-server_1.3-0.4.1-SNAPSHOTS.jar:$MAD_HOME/lib/* edp.mad.MadStarter &
-
-
-
