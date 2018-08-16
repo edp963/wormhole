bf15a83166911066b1da4e13ee8bcc8d35bc1af1
zp <85016910@qq.com>
2018-08-16 16:28:23 +0800

    interface

--- interface/publicinterface/src/main/scala/edp/wormhole/publicinterface/sinks/SinkProcessConfig.scala
+++ interface/publicinterface/src/main/scala/edp/wormhole/publicinterface/sinks/SinkProcessConfig.scala
@@ -1,8 +1,3 @@
-package edp.wormhole.sparkxinterface.batchjob.source
-
-import edp.wormhole.util.config.ConnectionConfig
-import org.apache.spark.sql.{DataFrame, SparkSession}
-
 /*-
  * <<
  * wormhole
@@ -12,9 +7,9 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,6 +18,16 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
  * >>
  */
 
-trait ObtainSourceDataInterface {
-  def process(session: SparkSession, startTime: String, endTime: String, sourceNamespace: String,  connectionConfig: ConnectionConfig, specialConfig: Option[String]): DataFrame
+
+package edp.wormhole.publicinterface.sinks
+
+case class SinkProcessConfig(sinkOutput:String,
+                             tableKeys: Option[String],
+                             specialConfig: Option[String],
+                             jsonSchema:Option[String],
+                             classFullname: String,
+                             retryTimes: Int,
+                             retrySeconds: Int){
+  lazy val tableKeyList = if (tableKeys.isEmpty || tableKeys.get == null) Nil else tableKeys.get.split(",").map(_.trim.toLowerCase).toList
 }
+
