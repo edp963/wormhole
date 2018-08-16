38e9b3c01e54ae9418b6ad5aa68b434ffa584c2b
sunsen <sensun@creditease.cn>
2018-08-16 16:01:21 +0800

    change hadoop package name

--- sparkx/src/main/scala/edp/wormhole/batchjob/source/HdfsLogReadUtil.scala
+++ sparkx/src/main/scala/edp/wormhole/batchjob/source/HdfsLogReadUtil.scala
@@ -23,10 +23,10 @@ package edp.wormhole.batchjob.source
 
 import java.sql.Timestamp
 
-import edp.wormhole.common.hadoop.HdfsUtils
 import edp.wormhole.common.hadoop.HdfsUtils._
 import edp.wormhole.common.util.DateUtils
 import edp.wormhole.common.util.FileUtils._
+import edp.wormhole.externalclient.hadoop.HdfsUtils
 import edp.wormhole.spark.log.EdpLogging
 import edp.wormhole.ums.{UmsNamespace, UmsOpType, UmsProtocolType, UmsSysField}
 import org.apache.hadoop.conf.Configuration
