66655b1dadda757460091069c2fe573cb1b70831
zp <85016910@qq.com>
2018-08-16 16:28:23 +0800

    interface

--- interface/publicinterface/src/main/scala/edp/wormhole/publicinterface/config/KafkaConfig.scala
+++ interface/publicinterface/src/main/scala/edp/wormhole/publicinterface/config/KafkaConfig.scala
@@ -1,20 +1,15 @@
-package edp.wormhole.sparkxinterface.batchjob.source
-
-import edp.wormhole.util.config.ConnectionConfig
-import org.apache.spark.sql.{DataFrame, SparkSession}
-
 /*-
  * <<
  * wormhole
  * ==
- * Copyright (C) 2016 - 2017 EDP
+ * Copyright (C) 2016 - 2018 EDP
  * ==
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,6 +18,13 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
  * >>
  */
 
-trait ObtainSourceDataInterface {
-  def process(session: SparkSession, startTime: String, endTime: String, sourceNamespace: String,  connectionConfig: ConnectionConfig, specialConfig: Option[String]): DataFrame
-}
+package edp.wormhole.publicinterface.config
+
+case class KafkaConfig(format: Option[String] = None,
+                       preserveSystemField: Option[Boolean] = None,
+                       `batch_size`: Option[Int] = None,
+                       sinkKafkaTopic:Option[String]) {
+                         lazy val messageFormat = format.getOrElse("ums")
+                         lazy val limitNum = `batch_size`.getOrElse(50)
+                         lazy val hasSystemField = preserveSystemField.getOrElse(false)
+                       }
