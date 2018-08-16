143f04a9c9cce3ff3bf1fd10f4a00797efbbef41
sunsen <sensun@creditease.cn>
2018-08-16 19:08:36 +0800

    重构interface

--- interface/publicinterface/src/main/scala/edp/wormhole/publicinterface/config/KafkaConfig.scala
+++ interface/publicinterface/src/main/scala/edp/wormhole/publicinterface/config/KafkaConfig.scala
@@ -18,13 +18,13 @@
  * >>
  */
 
-package edp.wormhole.publicinterface.config
-
-case class KafkaConfig(format: Option[String] = None,
-                       preserveSystemField: Option[Boolean] = None,
-                       `batch_size`: Option[Int] = None,
-                       sinkKafkaTopic:Option[String]) {
-                         lazy val messageFormat = format.getOrElse("ums")
-                         lazy val limitNum = `batch_size`.getOrElse(50)
-                         lazy val hasSystemField = preserveSystemField.getOrElse(false)
-                       }
+//package edp.wormhole.publicinterface.config
+//
+//case class KafkaConfig(format: Option[String] = None,
+//                       preserveSystemField: Option[Boolean] = None,
+//                       `batch_size`: Option[Int] = None,
+//                       sinkKafkaTopic:Option[String]) {
+//                         lazy val messageFormat = format.getOrElse("ums")
+//                         lazy val limitNum = `batch_size`.getOrElse(50)
+//                         lazy val hasSystemField = preserveSystemField.getOrElse(false)
+//                       }
