ff975b19a9912111648718d68892a15a22c33623
sunsen <sensun@creditease.cn>
2018-08-23 15:22:16 +0800

    refactor lookup kudu

--- flinkx/src/main/scala/edp/wormhole/flinkx/swifts/LookupMapper.scala
+++ flinkx/src/main/scala/edp/wormhole/flinkx/swifts/LookupMapper.scala
@@ -20,8 +20,7 @@
 
 package edp.wormhole.flinkx.swifts
 
-import edp.wormhole.flinkx.swifts.custom.LookupHbaseHelper
-import edp.wormhole.flinkx.swifts.custom.LookupRedisHelper
+import edp.wormhole.flinkx.swifts.custom.{LookupHbaseHelper, LookupKuduHelper}
 import edp.wormhole.ums.UmsDataSystem
 import edp.wormhole.util.config.ConnectionConfig
 import edp.wormhole.util.swifts.SwiftsSql
@@ -32,7 +31,7 @@ import org.apache.flink.types.Row
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
-class LookupMapper(swiftsSql: SwiftsSql, preSchemaMap: Map[String, (TypeInformation[_], Int)],dbOutPutSchemaMap: Map[String, (String, String, Int)], dataStoreConnectionsMap: Map[String, ConnectionConfig]) extends RichMapFunction[Row, Seq[Row]] with java.io.Serializable {
+class LookupMapper(swiftsSql: SwiftsSql, preSchemaMap: Map[String, (TypeInformation[_], Int)], dbOutPutSchemaMap: Map[String, (String, String, Int)], dataStoreConnectionsMap: Map[String, ConnectionConfig]) extends RichMapFunction[Row, Seq[Row]] with java.io.Serializable {
 
   private val sourceTableFields: Array[String] = if (swiftsSql.sourceTableFields.isDefined) swiftsSql.sourceTableFields.get else null
   private val lookupTableFields = if (swiftsSql.lookupTableFields.isDefined) swiftsSql.lookupTableFields.get else null
@@ -42,14 +41,13 @@ class LookupMapper(swiftsSql: SwiftsSql, preSchemaMap: Map[String, (TypeInformat
   override def map(value: Row): Seq[Row] = {
     val lookupNamespace: String = if (swiftsSql.lookupNamespace.isDefined) swiftsSql.lookupNamespace.get else null
     val lookupDataMap: mutable.HashMap[String, ListBuffer[Array[Any]]] = UmsDataSystem.dataSystem(lookupNamespace.split("\\.")(0).toLowerCase()) match {
-      case UmsDataSystem.HBASE=>LookupHbaseHelper.covertResultSet2Map(swiftsSql, value, preSchemaMap,dbOutPutSchemaMap,sourceTableFields,dataStoreConnectionsMap)
-      case UmsDataSystem.REDIS=>LookupRedisHelper.covertResultSet2Map(swiftsSql, value, preSchemaMap,dataStoreConnectionsMap)
-      case _=>LookupHelper.covertResultSet2Map(swiftsSql, value, preSchemaMap,dataStoreConnectionsMap)
+      case UmsDataSystem.HBASE => LookupHbaseHelper.covertResultSet2Map(swiftsSql, value, preSchemaMap, dbOutPutSchemaMap, sourceTableFields, dataStoreConnectionsMap)
+      case UmsDataSystem.KUDU => LookupKuduHelper.covertResultSet2Map(swiftsSql, value, preSchemaMap, dataStoreConnectionsMap)
+      case _ => LookupHelper.covertResultSet2Map(swiftsSql, value, preSchemaMap, dataStoreConnectionsMap)
     }
-    val joinFields =UmsDataSystem.dataSystem(lookupNamespace.split("\\.")(0).toLowerCase()) match{
-      case UmsDataSystem.HBASE=>LookupHbaseHelper.joinFieldsInRow(value, lookupTableFields, sourceTableFields, preSchemaMap).mkString("_")
-      case UmsDataSystem.REDIS=>LookupRedisHelper.joinFieldsInRow(value, swiftsSql, preSchemaMap)
-      case _=>LookupHelper.joinFieldsInRow(value, lookupTableFields, sourceTableFields, preSchemaMap).mkString("_")
+    val joinFields = UmsDataSystem.dataSystem(lookupNamespace.split("\\.")(0).toLowerCase()) match {
+      case UmsDataSystem.HBASE => LookupHbaseHelper.joinFieldsInRow(value, lookupTableFields, sourceTableFields, preSchemaMap).mkString("_")
+      case _ => LookupHelper.joinFieldsInRow(value, lookupTableFields, sourceTableFields, preSchemaMap).mkString("_")
     }
 
     if (lookupDataMap == null || !lookupDataMap.contains(joinFields)) {
