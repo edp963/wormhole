c55ecbe83bc0b214256d3ccc13ef3a35938e3101
sunsen <sensun@creditease.cn>
2018-11-13 17:18:56 +0800

    add file from upstream master

--- flinkx/src/main/scala/edp/wormhole/flinkx/eventflow/UmsProcessElement.scala
+++ flinkx/src/main/scala/edp/wormhole/flinkx/eventflow/UmsProcessElement.scala
@@ -15,9 +15,10 @@ import org.joda.time.DateTime
 
 import scala.collection.mutable.ArrayBuffer
 
-class UmsProcessElement(sourceSchemaMap: Map[String, (TypeInformation[_], Int)], exceptionConfig: ExceptionConfig, jsonSourceParseMap: Map[(UmsProtocolType, String), (Seq[UmsField], Seq[FieldInfo], ArrayBuffer[(String, String)])], kafkaDataTag: OutputTag[String]) extends ProcessFunction[(String, String, String, Int, Long), Row]{
+class UmsProcessElement(sourceSchemaMap: Map[String, (TypeInformation[_], Int)], exceptionConfig: ExceptionConfig, jsonSourceParseMap: Map[(UmsProtocolType, String), (Seq[UmsField], Seq[FieldInfo], ArrayBuffer[(String, String)])], kafkaDataTag: OutputTag[String]) extends ProcessFunction[(String, String, String, Int, Long), Row] {
   //private val outputTag = OutputTag[String]("kafkaDataException")
   private lazy val logger = Logger.getLogger(this.getClass)
+
   override def processElement(value: (String, String, String, Int, Long), ctx: ProcessFunction[(String, String, String, Int, Long), Row]#Context, out: Collector[Row]): Unit = {
     logger.info("in UmsFlatMapper source data from kafka " + value._2)
     try {
@@ -39,23 +40,13 @@ class UmsProcessElement(sourceSchemaMap: Map[String, (TypeInformation[_], Int)],
       }
     } catch {
       case ex: Throwable =>
-<<<<<<< HEAD
         logger.error("in doFlinkSql table query", ex)
         //out.collect(new Row(0))
         ctx.output(kafkaDataTag, UmsProtocolUtils.feedbackFlowFlinkxError(exceptionConfig.sourceNamespace, exceptionConfig.streamId, exceptionConfig.flowId, exceptionConfig.sinkNamespace, new DateTime(), value._2, ex.getMessage))
-=======
-        ex.printStackTrace()
-        out.collect(new Row(0))
-<<<<<<< HEAD
-        ctx.output(kafkaDataTag, UmsProtocolUtils.feedbackFlowFlinkxError(namespaceIdConfig.sourceNamespace, namespaceIdConfig.streamId, namespaceIdConfig.flowId, namespaceIdConfig.sinkNamespace, new DateTime(), value._2, ex.getMessage))
->>>>>>> print exception to log
-=======
-        ctx.output(kafkaDataTag, UmsProtocolUtils.feedbackFlowFlinkxError(exceptionConfig.sourceNamespace, exceptionConfig.streamId, exceptionConfig.flowId, exceptionConfig.sinkNamespace, new DateTime(), value._2, ex.getMessage))
->>>>>>> add exception process method
     }
   }
 
-  def createRow(tuple: Seq[String], protocolType:String, out: Collector[Row]): Unit = {
+  def createRow(tuple: Seq[String], protocolType: String, out: Collector[Row]): Unit = {
     val row = new Row(tuple.size)
     for (i <- tuple.indices)
       row.setField(i, FlinkSchemaUtils.getRelValue(i, tuple(i), sourceSchemaMap))
@@ -63,3 +54,4 @@ class UmsProcessElement(sourceSchemaMap: Map[String, (TypeInformation[_], Int)],
   }
 
 }
+
