cf01a29f7538ab6e6f5a2a7a575a2997f4fd3f75
guoyinxing <gyx1986gyx@126.com>
2018-12-03 18:59:29 +0800

    add kerberos support for wormhole

--- rider/rider-server/src/main/scala/edp/rider/common/RiderConfig.scala
+++ rider/rider-server/src/main/scala/edp/rider/common/RiderConfig.scala
@@ -356,7 +356,7 @@ object RiderConfig {
 
   lazy val defaultFlinkConfig = FlinkDefaultConfig("", FlinkResourceConfig(2, 6, 1, 2), "")
 
-  lazy val flink = RiderFlink(config.getString("flink.home"), config.getString("flink.yarn.queue.name"),config.getString("flink.feedback.state.count").toInt,config.getString("flink.feedback.interval").toInt, 1, 1,
+  lazy val flink = RiderFlink(config.getString("flink.home"), config.getString("flink.yarn.queue.name"),getIntConfig("flink.feedback.state.count",100),getIntConfig("flink.feedback.interval",30), 1, 1,
     getStringConfig("flink.wormhole.jar.path", s"${RiderConfig.riderRootPath}/lib/wormhole-ums_1.3-flinkx_1.5.1-0.5.6-beta-jar-with-dependencies.jar"),
     getStringConfig("flink.wormhole.client.log.path", s"$riderRootPath/logs/flows"),
     getIntConfig("spark.kafka.session.timeout", 30000),
