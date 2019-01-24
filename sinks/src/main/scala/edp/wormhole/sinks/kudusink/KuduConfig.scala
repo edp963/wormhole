a6fdc9222b8365a65272d2070164a8fe25ca5050
Xiaoyan <1223117836@qq.com>
2019-01-24 15:51:08 +0800

    kudu sink and lookup optimization

--- sinks/src/main/scala/edp/wormhole/sinks/kudusink/KuduConfig.scala
+++ sinks/src/main/scala/edp/wormhole/sinks/kudusink/KuduConfig.scala
@@ -5,5 +5,5 @@ import edp.wormhole.sinks.SourceMutationType
 case class KuduConfig(`mutation_type`:Option[String] = None,
                  `batch_size`: Option[Int] = None){
   lazy val `mutation_type.get` = `mutation_type`.getOrElse(SourceMutationType.I_U_D.toString)
-  lazy val `batch_size.get` = `batch_size`.getOrElse(1000)
+  lazy val `batch_size.get` = `batch_size`.getOrElse(50000)
 }
