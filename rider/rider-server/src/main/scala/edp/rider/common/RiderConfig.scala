6c91610829e40e991bfd6993006cf140ae09768d
liuwenli <wenliliu6@creditease.cn>
2019-03-08 14:04:07 +0800

    update flinkx jar path

--- rider/rider-server/src/main/scala/edp/rider/common/RiderConfig.scala
+++ rider/rider-server/src/main/scala/edp/rider/common/RiderConfig.scala
@@ -366,7 +366,7 @@ object RiderConfig {
   lazy val defaultFlinkConfig = FlinkDefaultConfig("", FlinkResourceConfig(2, 6, 1, 2), "")
 
   lazy val flink = RiderFlink(config.getString("flink.home"), config.getString("flink.yarn.queue.name"), getBooleanConfig("flink.feedback.enabled", false), getIntConfig("flink.feedback.state.count", 100), getIntConfig("flink.feedback.interval", 30), 1, 1,
-    getStringConfig("flink.wormhole.jar.path", s"${RiderConfig.riderRootPath}/app/wormhole-ums_1.3-flinkx_1.5.1-0.6.0-jar-with-dependencies.jar"),
+    getStringConfig("flink.wormhole.jar.path", s"${RiderConfig.riderRootPath}/app/wormhole-ums_1.3-flinkx_1.7.2-0.6.0-jar-with-dependencies.jar"),
     getStringConfig("flink.wormhole.client.log.path", s"$riderRootPath/logs/flows"),
     getIntConfig("spark.kafka.session.timeout", 30000),
     getIntConfig("spark.kafka.group.max.session.timeout.ms", 60000)
