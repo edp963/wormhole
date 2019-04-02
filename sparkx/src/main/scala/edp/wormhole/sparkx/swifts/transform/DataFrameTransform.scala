18e7871a4b814f8c17ee26dc61836bd690f19d26
liuwenli <wenliliu6@creditease.cn>
2019-04-02 16:22:53 +0800

    fix union kudu null bug

--- sparkx/src/main/scala/edp/wormhole/sparkx/swifts/transform/DataFrameTransform.scala
+++ sparkx/src/main/scala/edp/wormhole/sparkx/swifts/transform/DataFrameTransform.scala
@@ -297,7 +297,13 @@ object DataFrameTransform extends EdpLogging {
                 }
                 (DateUtils.yyyyMMddHHmmss(newValue),UmsFieldType.DATETIME)
               }else{
-                (tmpValue._1.toString,umsFieldType(tmpValue._2))
+                val newTmpValue=umsFieldType(tmpValue._2) match {
+                  case STRING => if(tmpValue._1==null) "" else tmpValue._1.toString
+                  case INT | LONG |  FLOAT | DOUBLE | DECIMAL | BINARY => if(tmpValue._1==null) "0" else tmpValue._1.toString
+                  case DATE| DATETIME =>if(tmpValue._1==null) DateUtils.currentyyyyMMddHHmmss else tmpValue._1.toString
+                  case BOOLEAN => if(tmpValue._1==null) "false" else tmpValue._1.toString
+                }
+                (newTmpValue,umsFieldType(tmpValue._2))
               }
               logInfo(s"fieldValue:$fieldValue fieldType:$fieldType")
               outputArray.append(
