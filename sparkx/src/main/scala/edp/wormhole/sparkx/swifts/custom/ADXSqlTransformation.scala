34fb3886494c0ef6c38d8359d0e46d4580874dd1
pei <guoqiangpei@creditease.cn>
2019-06-03 15:43:19 +0800

    auto add  fields

---
+++ sparkx/src/main/scala/edp/wormhole/sparkx/swifts/custom/ADXSqlTransformation.scala
@@ -0,0 +1,45 @@
+package edp.wormhole.sparkx.swifts.custom
+
+import edp.wormhole.sparkxinterface.swifts.{SwiftsInterface, SwiftsProcessConfig}
+import edp.wormhole.util.swifts.SwiftsSql
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+
+
+/**
+  * Created by neo_qiang on 2019/5/29.
+  */
+class ADXSqlTransformation extends SwiftsInterface{
+    override def transform(session: SparkSession, df: DataFrame, config: SwiftsProcessConfig): DataFrame = {
+      var temp_df:DataFrame = df
+      //Get Schema From Transformation
+      val schemasFromSwifts = config.swiftsSql match {
+        case Some(array)=> getFieldsfromSwifts(array(1))
+        case None => Seq.empty[String]
+      }
+      //Get Schema From Sprek Stream
+      val schemasFromStream:Seq[String] = df.columns.toSeq
+      //Scan schemasFromStream To add  Column (type:String , value:null)
+      schemasFromSwifts.foreach(schema => {
+        if(config.datasetShow.get) println(s"check Column $schema")
+        if(!(schema.equals("ums_uid_")||schema.equals("ums_ts_")||schema.equals("ums_op_")||schema.equals("ums_id_"))&&schemasFromStream.indexOf(schema)<0) {
+          if(config.datasetShow.get) println(s"add Column with name : $schema")
+          temp_df = df.withColumn(schema,lit(null))
+        }
+      })
+      temp_df
+  }
+
+
+  def getFieldsfromSwifts(switsSql:SwiftsSql):Seq[String] = {
+    val sql = switsSql.sql
+    val from_index = sql.toLowerCase().lastIndexOf(" from ")
+    val field_str  = sql.trim.substring("select ".length,from_index)
+    field_str.split(",").map(f => {
+      val index_field_start = f.toLowerCase.lastIndexOf(" as ")
+      if(index_field_start<0) f.trim else f.substring(index_field_start+3,f.length).trim
+    }).toSeq
+  }
+
+}
