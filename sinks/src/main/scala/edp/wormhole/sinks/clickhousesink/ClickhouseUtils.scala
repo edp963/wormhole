f069e9f7164083fe8076e1fc83970f22eea17975
Liuwenli11 <liuwenli11@sina.cn>
2019-08-21 21:30:10 +0800

    update clickhouse xxhash64

--- sinks/src/main/scala/edp/wormhole/sinks/clickhousesink/ClickhouseUtils.scala
+++ sinks/src/main/scala/edp/wormhole/sinks/clickhousesink/ClickhouseUtils.scala
@@ -1,9 +1,11 @@
 package edp.wormhole.sinks.clickhousesink
 
+import java.nio.ByteBuffer
+
 import edp.wormhole.sinks.utils.SinkCommonUtils
 import edp.wormhole.ums.UmsFieldType.UmsFieldType
 import edp.wormhole.util.config.ConnectionConfig
-import net.openhft.hashing.LongHashFunction
+import net.jpountz.xxhash.{XXHash64, XXHashFactory}
 import org.apache.log4j.Logger
 
 import scala.collection.mutable
@@ -13,15 +15,14 @@ object ClickhouseUtils {
   private lazy val logger = Logger.getLogger(this.getClass)
   def getTuplesShardMap(tupleList: Seq[Seq[String]], connectionConfig: ConnectionConfig, keyList: List[String], schemaMap: collection.Map[String, (Int, UmsFieldType, Boolean)]): Map[ConnectionConfig, Seq[Seq[String]]] = {
     val shardServers = connectionConfig.connectionUrl.split(",")
-    val hashFunction = LongHashFunction.xx()
+    /*val hashFunction = LongHashFunction.xx()
     val shardServersSorted = shardServers.sortWith((left, right) => {
       hashFunction.hashChars(getIpPort(left)) < hashFunction.hashChars(getIpPort(right))
-    }) //取ip:port从小到大排序
-
+    }) //取ip:port从小到大排序*/
     val sharpTupleMap = mutable.HashMap.empty[ConnectionConfig, mutable.ListBuffer[Seq[String]]]
     tupleList.foreach(tuple => {
-      val shardValue = SinkCommonUtils.keyList2values(keyList, schemaMap, tuple)
-      val curShardUrl = selectShardUrl(shardValue, shardServersSorted)
+      val shardKey = SinkCommonUtils.keyList2values(keyList, schemaMap, tuple)
+      val curShardUrl = shardServers(selectShardNum(shardKey, shardServers.size))
       val shardConnection = ConnectionConfig(curShardUrl, connectionConfig.username, connectionConfig.password, connectionConfig.parameters)
       val curTuples = sharpTupleMap.getOrElse(shardConnection, ListBuffer.empty[Seq[String]])
       curTuples.append(tuple)
@@ -30,13 +31,25 @@ object ClickhouseUtils {
     sharpTupleMap.toMap
   }
 
-  def selectShardUrl(shardValue: String, shardServersSorted: Seq[String]): String = {
-    val hashFunction = LongHashFunction.xx()
-    val hashNum = math.abs(hashFunction.hashChars(shardValue))
-    shardServersSorted((hashNum % shardServersSorted.size).toInt)
-  }
-  def getIpPort(jdbcUrl: String): String = {
-    jdbcUrl.split("//")(1).split("/")(0)
+  def selectShardNum(shardKey: String, shardServersSize: Int): Int = {
+    val hasher = XXHashFactory.fastestInstance.hash64
+    val longHashNum = hasher.hash(ByteBuffer.wrap(shardKey.getBytes()), 0)
+    val uInt64HashNum = long2UInt64(longHashNum)
+    val shardNum = uInt64HashNum % shardServersSize
+    logger.info(s"shardKey: $shardKey, longHashNum: $longHashNum, uInt64HashNum $uInt64HashNum, shardNum: $shardNum")
+    shardNum.toInt
   }
 
+  def long2UInt64(longValue: Long): BigDecimal = {
+    if(longValue >= 0) {
+      BigDecimal(longValue)
+    } else {
+      val lowValue = longValue & 0x7fffffffffffffffL
+      BigDecimal(lowValue) + BigDecimal.valueOf(Long.MaxValue) + BigDecimal.valueOf(1)
+    }
+  }
+
+/*  def getIpPort(jdbcUrl: String): String = {
+    jdbcUrl.split("//")(1).split("/")(0)
+  }*/
 }
