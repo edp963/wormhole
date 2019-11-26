75bde8a4b40fc96f2cd959a30644e4187a59fc9f
kelv2 <kelv2@creditease.cn>
2019-11-26 14:53:51 +0800

    sensor data

--- sparkx/src/main/scala/edp/wormhole/sparkx/swifts/custom/sensors/SensorsDataTransform.scala
+++ sparkx/src/main/scala/edp/wormhole/sparkx/swifts/custom/sensors/SensorsDataTransform.scala
@@ -3,12 +3,11 @@ package edp.wormhole.sparkx.swifts.custom.sensors
 import java.io.Serializable
 import java.util
 
-import edp.wormhole.sparkx.common.WormholeConfig
 
 import scala.collection.JavaConversions._
 import edp.wormhole.sparkx.swifts.custom.sensors.ase.AESUtil
 import edp.wormhole.sparkx.swifts.custom.sensors.entry.{EventEntry, PropertyColumnEntry}
-import edp.wormhole.sparkxinterface.swifts.{SwiftsInterface, SwiftsProcessConfig}
+import edp.wormhole.sparkxinterface.swifts.{SwiftsInterface, SwiftsProcessConfig, WormholeConfig}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
 import org.apache.spark.sql.types._
@@ -24,10 +23,10 @@ import scala.collection.mutable.ArrayBuffer
   *  19/11/14 15:35
   *       To change this template use File | Settings | File Templates.
   */
-class SensorsDataTransform extends Serializable {
+class SensorsDataTransform extends SwiftsInterface {
 
 
-   def transform(session: SparkSession, df: DataFrame, config: SwiftsProcessConfig,param:String, streamConfig: WormholeConfig):DataFrame={
+   override def  transform(session: SparkSession, df: DataFrame, flowConfig: SwiftsProcessConfig,param:String,streamConfig: WormholeConfig):DataFrame={
     import session.sqlContext.implicits._
     if(param==null){
       throw new IllegalArgumentException("param must be not empty");
