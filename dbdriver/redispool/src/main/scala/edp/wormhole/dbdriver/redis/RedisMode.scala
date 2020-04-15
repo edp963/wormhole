cd33350069bb8ca1c8c2d6c267291428c893dc7d
sunsen <sensun@creditease.cn>
2020-04-15 10:14:57 +0800

    add redis sentinel mode

---
+++ dbdriver/redispool/src/main/scala/edp/wormhole/dbdriver/redis/RedisMode.scala
@@ -0,0 +1,22 @@
+package edp.wormhole.dbdriver.redis
+
+import org.apache.log4j.Logger
+
+
+object RedisMode extends Enumeration {
+  private lazy val logger = Logger.getLogger(this.getClass)
+  type RedisMode = Value
+
+  val SHARED = Value("shared")
+  val CLUSTER = Value("cluster")
+  val SENTINEL = Value("sentinel")
+
+  def redisMode(s: String): RedisMode = try {
+    RedisMode.withName(s.toLowerCase)
+  } catch {
+    case e: Throwable =>
+      logger.warn(s"RedisMode invalid string: $s",e)
+      SENTINEL
+  }
+
+}
